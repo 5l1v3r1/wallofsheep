c3745d8578fa26c502520c4cfbf195eb0a08442f
Che-Wei Lin <linton.tw@gmail.com>
2015-04-18 11:24:20 +0800

    Fix for felixge/node-style-guide

--- nodejs/sniffer.js
+++ nodejs/sniffer.js
@@ -1,48 +1,55 @@
 var nic = process.argv[2];
 var qs = require('querystring');
 var pcap = require('./node_modules/pcap'),
-    pcap_session = pcap.createSession(nic, "ip proto \\tcp");
+    pcap_session = pcap.createSession(nic, 'ip proto \\tcp');
 
-function getHTTPLoginAccount(data){
+function GetHTTPLoginAccount(data) {
     var account = 'None';
 
-    if (data.account){
+    if (data.account) {
         account = data.account;
-    } else if (data.identification){
+    } else if (data.identification) {
         account = data.identification;
-    } else if (data.username){
+    } else if (data.username) {
         account = data.username;
-    } else if (data.id){
+    } else if (data.id) {
         account = data.id;
-    } else if (data.os_username){
+    } else if (data.os_username) {
         account = data.os_username;
-    } else if (data.txtAccount){
+    } else if (data.txtAccount) {
         account = data.txtAccount;
-    } else if (data.email){
+    } else if (data.email) {
         account = data.email;
+    } else if (data.loginAccount) {
+        account = data.loginAccount;
     } else {
         console.log('[-] Can not find account pattern');
     }
     return account;
 }
 
-function getHTTPLoginPassword(data){
+function GetHTTPLoginPassword(data) {
     var password = 'None';
 
-    if (data.password){
+    if (data.password) {
         password = data.password;
-    } else if (data.os_password){
+    } else if (data.os_password) {
         password = data.os_password;
-    } else if (data.txtPwd){
+    } else if (data.txtPwd) {
         password = data.txtPwd;
+    } else if (data.loginPasswd) {
+        password = data.loginPasswd;
     } else {
         console.log('[-] Can not find password pattern');
     }
     return password;
 }
 
-function HTTPPostParser(packet){
+function HTTPPostParser(packet) {
     var data = packet.payload.payload.payload.data.toString('ascii');
+
+    // DEBUG usage
+    // console.log(data);
 
     // Source MAC address
     var shost = packet.payload.shost.toString('ascii');
@@ -59,11 +66,12 @@ function HTTPPostParser(packet){
     // Dst port
     var dport = packet.payload.payload.payload.dport.toString();
 
+    var isPOST = data.indexOf('POST');
     // If there is a POST method
-    if (data.indexOf("POST") > -1){
+    if (isPOST > -1) {
         // HTTP header with conten
         // console.log(data);
-        var headerContent = data.split("\r\n");
+        var headerContent = data.split('\r\n');
 
         // console.log(headerContent);
 
@@ -73,18 +81,18 @@ function HTTPPostParser(packet){
         var sheepInfo = qs.parse(lastContent);
 
         // For DEGUG print
-        // console.log(sheepInfo);
+        console.log(sheepInfo);
 
-        var account = getHTTPLoginAccount(sheepInfo);
-        var password = getHTTPLoginPassword(sheepInfo);
+        var account = GetHTTPLoginAccount(sheepInfo);
+        var password = GetHTTPLoginPassword(sheepInfo);
 
-        consolePrinter(shost, saddr, daddr, sport, dport, account, password);
+        ConsolePrinter(shost, saddr, daddr, sport, dport, account, password);
 
     }
 
 }
 
-function FTPloginParser(packet){
+function FTPloginParser(packet) {
     var data = packet.payload.payload.payload.data.toString('ascii');
 
     // Source MAC address
@@ -104,15 +112,18 @@ function FTPloginParser(packet){
 
     var ftp_user_re = /^USER (.*)$/;
     var ftp_pw_re = /^PASS (.*)$/;
-    var splitted = data.split("\r\n");
+    var splitted = data.split('\r\n');
 
-    if (splitted[0].indexOf("USER") > -1 || splitted[0].indexOf("PASS") > -1){
+    var isUSER = splitted[0].indexOf('USER');
+    var isPASS = splitted[0].indexOf('PASS');
+
+    if (isUSER > -1 || isPASS > -1) {
         var user = splitted[0].match(ftp_user_re);
         var pass = splitted[0].match(ftp_pw_re);
-        if (user !== null){
+        if (user !== null) {
             console.log('[-] user:' + user[1]);
         }
-        if (pass !== null){
+        if (pass !== null) {
             console.log('[-] pass:' + pass[1]);
         }
 
@@ -120,12 +131,12 @@ function FTPloginParser(packet){
 
 }
 
-function consolePrinter(shost, srcIP, dstIP, sport, dport, account, password){
+function ConsolePrinter(shost, srcIP, dstIP, sport, dport, account, password) {
     console.log('[' + srcIP + ':' +  sport + ' -> ' + dstIP + ':' +  dport + '] Account: ' + account);
     console.log('[' + srcIP + ':' +  sport + ' -> ' + dstIP + ':' +  dport + '] Password: ' + password);
 }
 
-if (!nic){
+if (!nic) {
     console.log('[*] Specify an interface name for capture.');
     process.exit(1);
 } else {
@@ -135,16 +146,21 @@ if (!nic){
 pcap_session.on('packet', function (raw_packet) {
 
     var packet = pcap.decode.packet(raw_packet);
-    //console.log(packet);
+    // console.log(packet);
 
-    // For source port dst port is 80 and data not null (i.e. All request packets)
-    if (packet.payload.payload.payload.dport === 80 && packet.payload.payload.payload.data !== null){
+    var isHTTP = packet.payload.payload.payload.dport === 80 && packet.payload.payload.payload.data !== null;
+    var isFTP = packet.payload.payload.payload.dport === 21  && packet.payload.payload.payload.data !== null;
+    var isPOP3 = packet.payload.payload.payload.dport === 110  && packet.payload.payload.payload.data !== null;
+    var isIMAP = packet.payload.payload.payload.dport === 143  && packet.payload.payload.payload.data !== null;
+
+    // For all protocol we interested and data not null
+    if (isHTTP) {
       HTTPPostParser(packet);
-    } else if (packet.payload.payload.payload.dport === 21  && packet.payload.payload.payload.data !== null){
+    } else if (isFTP) {
       FTPloginParser(packet);
-    } else if (packet.payload.payload.payload.dport === 110  && packet.payload.payload.payload.data !== null){
+    } else if (isPOP3) {
         // TODO parse POP3
-    } else if (packet.payload.payload.payload.dport === 143  && packet.payload.payload.payload.data !== null){
+    } else if (isIMAP) {
         // TODO parse IMAP
     } else {
 
